58e7aee35ccb6d077e0e4856c5e5e217380b7228
eyob6117 <eyobteklu911@gmail.com>
2021-06-17 11:13:58 -0700

    put char

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-putchar("Programming is like building a multilingual puzzle\n") return(0)
+#include<stdio.h>
+int main(void){
+    
+    putchar("Programming is like building a multilingual puzzle\n");
+    return(0);
+    }
