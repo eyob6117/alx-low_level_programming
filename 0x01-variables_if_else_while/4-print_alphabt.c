4ee01f0e5d981d8c4bff2bda8b55c2ea9188c6ee
eyob6117 <eyobteklu911@gmail.com>
2021-06-18 14:04:15 -0700

    fixing file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int x;
+x = 'a';
+for (; x <= 'z'; x++)
+{
+if( x == q && x== e)
+{
+continue;
+}
+else
+{
+putchar(x);
+}
+putchar('\n');
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+return (0);
+}
