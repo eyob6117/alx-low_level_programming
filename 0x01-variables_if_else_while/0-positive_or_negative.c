96f40650f0769ac36a90cc16107f1c2d01aa6e19
eyob6117 <eyobteklu911@gmail.com>
2021-06-18 11:23:46 -0700

    fixing file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,46 +13,19 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-# output function
-#
-# parameterdescs, a hash.
-#  function => "function name"
-#  parameterlist => @list of parameters
-#  parameterdescs => %parameter descriptions
-#  sectionlist => @list of sections
-#  sections => %section descriptions
 if (n > 0)
 {
 printf("%d is positive\n", n);
 }
-# output function
-#
-# parameterdescs, a hash.
-#  function => "function name"
-#  parameterlist => @list of parameters
-#  parameterdescs => %parameter descriptions
-#  sectionlist => @list of sections
-#  sections => %section descriptions
 if (n == 0)
 {
 printf("%d is zero\n", n);
-# output function
-#
-# parameterdescs, a hash.
-#  function => "function name"
-#  parameterlist => @list of parameters
-#  parameterdescs => %parameter descriptions
-#  sectionlist => @list of sections
-#  sections => %section descriptions
 if (n < 0)
 {
 printf("%d is negative\n", n);
 }
 /**
- * @c: This is longer description of C
- *
- * Description: You can use paragraphs to describe arguments
- * using this method.
+ *some discription files
  */
 return (0);
 }
