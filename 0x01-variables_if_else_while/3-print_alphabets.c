cffbdf483c0faeaa56a3c28e663acb76469e325f
eyob6117 <eyobteklu911@gmail.com>
2021-06-18 13:49:49 -0700

    fixing file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int x;
+int y;
+x = 'a';
+for (; x <= 'z'; x++)
+{
+putchar(x);
+}
+y = 'A';
+for (; y <= 'Z'; y++)
+{
+putchar(y);
+}
+/**
+ *
+ * Return: Always 0 (success)
+ */
+return (0);
+}
