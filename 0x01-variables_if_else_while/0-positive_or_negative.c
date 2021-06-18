09b331b4e035e2368a63813f7136a1590fe7818a
eyob6117 <eyobteklu911@gmail.com>
2021-06-18 03:11:56 -0700

    fixing file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,12 @@
 #include <time.h>
 /* more headers goes there */
 /* betty style doc for function main goes there */
+/**
+ * is_positive - Check if a number is greater than 0
+ * @nb: The number to be checked
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
 int main(void)
 {
 int n;
