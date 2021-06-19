8e87d5e2210c05c791794cfe2293df586ec14386
eyob6117 <eyobteklu911@gmail.com>
2021-06-19 00:25:28 -0700

    fixing files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int intType;
 float floatType;
 long int longType;
 long long longlongType;
-fprintf(stderr, "Size of char: %zu byte(s)\n", sizeof(charType));
-fprintf(stderr, "Size of int: %zu byte(s)\n", sizeof(intType));
-fprintf(stderr, "Size of long int: %zu byte(s)\n", sizeof(longType));
-fprintf(stderr, "Size of long long: %zu byte(s)\n", sizeof(longlongType));
-fprintf(stderr, "Size of float: %zu byte(s)\n", sizeof(floatType));
+fprintf(stderr, "Size of a char: %zu byte(s)\n", sizeof(charType));
+fprintf(stderr, "Size of an int: %zu byte(s)\n", sizeof(intType));
+fprintf(stderr, "Size of a long int: %zu byte(s)\n", sizeof(longType));
+fprintf(stderr, "Size of a long long: %zu byte(s)\n", sizeof(longlongType));
+fprintf(stderr, "Size of a float: %zu byte(s)\n", sizeof(floatType));
 /**
  * main - Entry point
  *
