301702560f22ee1a3422a349b5897b3fc4ba1bde
eyob6117 <eyobteklu911@gmail.com>
2021-06-19 12:36:07 -0700

    pring base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet in lowercase letters in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char digit;
+
+for (digit = '0'; digit <= '9'; digit++)
+putchar(digit);
+
+for (digit = 'a'; digit <= 'f'; digit++)
+putchar(digit);
+putchar('\n');
+/**
+ * main - Print the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+return (0);
