c747d4277208395e67426553360c7109b1506f32
eyob6117 <eyobteklu911@gmail.com>
2021-06-19 11:56:56 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i=0;
+while (i<10)
+{
+putchar(i);
+i++;
+}
+putchar("\n");
+/**
+ * main - Entry point
+ *
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: Always 0 (Success)
+ */
+return (0);
+}
