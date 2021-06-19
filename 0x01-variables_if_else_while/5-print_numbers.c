13ca15b6ed85356f781cb35e3cf70ca0826a7ba4
eyob6117 <eyobteklu911@gmail.com>
2021-06-19 11:38:39 -0700

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: Always 0 (Success)
+ */
+int main ()
+{
+for(int i = 0;i < 10;i++)
+{
+printf("%d", i);
+}
+printf("\n");
+/**
+ * main - Entry point
+ *
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: Always 0 (Success)
+ */
+return (0);
+}
