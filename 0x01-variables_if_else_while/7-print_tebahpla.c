7bf6a30d6ef404bd1927b953e52e50bb5bdfb96c
eyob6117 <eyobteklu911@gmail.com>
2021-06-19 12:21:29 -0700

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print all single digit numbers using only putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+for(ch = 'z';ch >= 'a';ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+/**
+ * main - Print all single digit numbers using only putchar
+ *
+ * Return: Always 0 (Success)
+ */
+return (0);
+}
