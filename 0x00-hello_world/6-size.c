136e2e5a6c85968d51fe1b893f8defa9301889cf
eyob6117 <eyobteklu911@gmail.com>
2021-06-19 00:41:49 -0700

    fixing files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,6 @@ printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-fprintf(stderr, "[Anything]");
 /**
  * main - Entry point
  *
