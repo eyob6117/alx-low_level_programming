536d000db4c01d521d7d2c10d8c4225b60261cd4
eyob6117 <eyobteklu911@gmail.com>
2021-06-19 02:14:50 -0700

    fixing file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,6 @@
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful""\""" - Dora Korpar, 2015-10-19\n");
-/**
- * main - Entry point
- *
- * Return: Always 1 (Sucess)
- */
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
