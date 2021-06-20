3179b8f76f0b7df95e503f73163e40eba3434900
eyob6117 <eyobteklu911@gmail.com>
2021-06-20 03:00:03 -0700

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,13 @@ int main(void)
 {
 int i;
 int j;
-for(i = 0; i < 9; i++)
+for (i = 0; i <= 9; i++)
 {
-for (j = 0; j < 9; j++)
+for (j = 0+1; j <= 9; j++)
 {
 putchar(i + '0');
 putchar(j + '0');
-if(i < 8)
+if (i < 8)
 {
 putchar(' ');
 putchar(',');
