6172380c662d978675c1a8408b64c5878302ba2c
eyob6117 <eyobteklu911@gmail.com>
2021-06-19 23:19:37 -0700

    comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,12 +14,14 @@ for (j = 0; j   < 9; j++)
 {
 putchar(i + '0');
 putchar(j + '0');
+}
 if(i < 8)
 {
 putchar(' ');
 putchar(',');
 } 
 putchar('\n');
+}
 /**
  * main - Print combinations of two digit numbers
  *
