15e205f8731e2a195eb56e842f414ac2fb4971fc
eyob6117 <eyobteklu911@gmail.com>
2021-06-20 04:21:26 -0700

    print 4

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Print all possible combinations of single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; i <=9; i++)
+{
+putchar(i + '0');
+putchar(j + '0');
+
+if (!(i == 9 && j == 9))
+{
+    
+putchar(',');
+putchar(' '); 
+ 
+}
+}
+}
+putchar('\n');
+/**
+ * main - Print numbers from 00 to 99
+ *
+ * Return: Always 0 (Success)
+ */
+return (0);
+ 
