566d6d0d558ec614a2dbd4a25de1aac742925198
eyob6117 <eyobteklu911@gmail.com>
2021-06-19 23:11:53 -0700

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+/**
+ * main - Print all possible combinations of single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+for(i = 0; i < 9; i++)
+{
+for (j = 0; j   < 9; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if(i < 8)
+{
+putchar(' ');
+putchar(',');
+} 
+putchar('\n');
+/**
+ * main - Print combinations of two digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+return (0);
+}
