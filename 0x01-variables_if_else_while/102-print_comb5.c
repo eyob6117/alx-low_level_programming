2cff5a64a27ba758a147276d129f9d2481479d27
eyob6117 <eyobteklu911@gmail.com>
2021-06-20 04:27:34 -0700

    print 4

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,17 +10,17 @@ int i;
 int j;
 for (i = 0; i <= 9; i++)
 {
-for (j = 0; i <=9; i++)
+for (j = 0; i <= 9; i++)
 {
 putchar(i + '0');
 putchar(j + '0');
 
 if (!(i == 9 && j == 9))
 {
-    
+
 putchar(',');
-putchar(' '); 
- 
+putchar(' ');
+
 }
 }
 }
