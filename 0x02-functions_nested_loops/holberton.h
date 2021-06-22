6b255c3d27a381a8c1fd0caf618122fef7566c1a
eyob6117 <eyobteklu911@gmail.com>
2021-06-22 12:26:16 -0700

    new file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -20,3 +20,4 @@ void print_times_table(int n);
 void print_spaces(int n);
 void print_zero(void);
  
+#endif
