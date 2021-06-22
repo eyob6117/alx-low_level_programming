38c074a4259e0a97bff3b19ca4ef8c92b85318fc
eyob6117 <eyobteklu911@gmail.com>
2021-06-22 10:42:47 -0700

    holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+putchar("Holberton");
+ /**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+  return (0);
+}
