664725c9624be098a0baf2fddfac6a6fc2fd566e
eyob6117 <eyobteklu911@gmail.com>
2021-06-22 11:38:36 -0700

    new file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char ch  = 'a';
+int i = 0;
+while(ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+}
+int main(void)
+{
+  print_alphabet();
+  return (0);
+}
