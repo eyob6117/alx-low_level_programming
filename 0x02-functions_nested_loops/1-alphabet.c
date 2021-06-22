fb445d22c11c4d512d770d2693eb73a3253af27b
eyob6117 <eyobteklu911@gmail.com>
2021-06-22 11:45:05 -0700

    new file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,16 +6,10 @@
  */
 void print_alphabet(void)
 {
-char ch  = 'a';
-while(ch <= 'z')
-{
-putchar(ch);
-ch++;
-}
-putchar('\n');
-}
-int main(void)
-{
-  print_alphabet();
-  return (0);
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
+_putchar('\n');
 }
