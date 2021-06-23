b7492a2ac69b4031f2df5f99abd911bb50f4a9bb
eyob6117 <eyobteklu911@gmail.com>
2021-06-23 09:48:12 -0700

    new file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always sucess(0)
+ */
+int main()
+{
+int a;
+int b = 1;
+int res;
+int i;
+for(i = 0; i < 50; i++)
+{
+putchar(a + '0');
+res = a + b;
+a = b;
+b = res;
+}
+return (0);
+}
