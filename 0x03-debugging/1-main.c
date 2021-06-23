92fce3fa374bfff8ea23d2f5bc1f20f70406e320
eyob6117 <eyobteklu911@gmail.com>
2021-06-23 12:55:50 -0700

    adding file

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,10 +11,11 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-/*while (i < 10)
-/*{
-/*putchar(i);
-}*/
+/**
+ * while (i < 10)
+ * {
+ * putchar(i);
+ */
 printf("Infin
 ite loop avoided! \\o/\n");
 
