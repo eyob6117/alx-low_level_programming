e22aadb3a283dc519a0ba851988a4157331e252a
eyob6117 <eyobteklu911@gmail.com>
2021-06-23 12:24:45 -0700

    adding new file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,16 @@
 #include "holberton.h"
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-positive_or_negative(n);
+int i;
+
+i = 0;
+positive_or_negative(i);
+
 return (0);
 }
--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -2,6 +2,6 @@
 #define HOLBERTON_H
 #include <stdio.h>
 
-void positive_or_negative(int n);
+void positive_or_negative(int i);
 
 #endif /* HOLBERTON_H */
