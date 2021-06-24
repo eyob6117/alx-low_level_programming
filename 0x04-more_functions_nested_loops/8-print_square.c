0850eb3456c290b9513e6e2b1b05457e9f693bcb
eyob6117 <eyobteklu911@gmail.com>
2021-06-24 11:01:45 -0700

    adding new file

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * print_square - print a square
+ * print_square - function to print a square
  * @size: integer value
  */
 
@@ -9,7 +9,7 @@ void print_square(int size)
 {
 int i, j;
 
-if (size < 0)
+if (size <= 0)
 {
 for (i = 1; i < size; i++)
 {
@@ -17,7 +17,7 @@ for (j = 1; j < size; j++)
 {
 _putchar('#');
 }
-
+_putchar('\n');
 }
 }
 _putchar('\n');
