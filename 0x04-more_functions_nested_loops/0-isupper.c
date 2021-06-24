5b1a3cf9fc7c3bee9243478cbd6e33ef89aef23b
eyob6117 <eyobteklu911@gmail.com>
2021-06-24 05:21:23 -0700

    adding new file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,11 +5,14 @@
  *
  * Return: 1 or 0
  */
-int main(void)
+int is_upper(int c)
 {
-is_upper(int c);
 if (isupper(c))
 return (1);
 else
+return (0);
+int main(void)
+{
+is_upper(int c);
 return (0);
 }
