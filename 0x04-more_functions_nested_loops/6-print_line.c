c9120c31991088c8716a932082b080d5c3b61e73
eyob6117 <eyobteklu911@gmail.com>
2021-06-24 09:32:16 -0700

    adding new files

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_line - draw a straight line in the terminal
+ */
+void print_line(int n);
+for (n = 0; n <= 5; n++)
+{
+putchar('-');
+if(n <= 0)
+{
+putcha('\n');
+}
+}
