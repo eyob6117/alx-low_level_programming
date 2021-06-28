ef8ead08341439d2ace98e197c0cd7b57f700fc4
eyob6117 <eyobteklu911@gmail.com>
2021-06-28 05:20:52 -0700

    tasks on pointer

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * puts - function that prints every other character of a string,with the
+ *    first charcter.
+ * @str: pointer variable str
+ */
+void puts(char *str)
+{
+int len = strlen(str);
+int i;
+for (i = 0; i < len; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
