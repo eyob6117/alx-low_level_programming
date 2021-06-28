2b0be4b548c9cb6d89608aa22241947f80242a00
eyob6117 <eyobteklu911@gmail.com>
2021-06-28 07:20:38 -0700

    tasks on pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strcpy - function that copies the strin pointed to by src including
+ *    including the terminatin null byte.
+ * @dest: char pointer variable
+ * @src: char pointer variable
+ */
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest,src);
+return (dest);
+}
