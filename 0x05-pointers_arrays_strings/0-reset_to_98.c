53c98d2117b447d1034aa1ac8dc65ca28f1df9c7
eyob6117 <eyobteklu911@gmail.com>
2021-06-27 23:56:26 -0700

    tasks on pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * reset-to-98 -takes a pointer and updates the value to 98
+ * reset-to-98 - takes a pointer and updates the value to 98
  * @n: integer variable n
- * 
+ *
  */
 void reset_to_98(int *n)
 {
 *n = 98;
-}  
+}
