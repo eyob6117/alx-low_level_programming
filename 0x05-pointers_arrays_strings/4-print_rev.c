84b4de899cee7a7c6e5fdbf60d072740186f2472
eyob6117 <eyobteklu911@gmail.com>
2021-06-28 02:44:00 -0700

    tasks on pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,8 +8,11 @@ void print_rev(char *s)
 {
 int len = strlen(s)-1;
 int i;
+char temp;
 for (i = len; i >= len / 2 ; i--)
-char temp = s[i];
+{
+temp = s[i];
 s[i] = s[len -i];
 s[len-i]=temp;
+}
 }
