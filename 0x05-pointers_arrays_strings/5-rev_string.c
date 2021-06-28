88285a62c230288940cae9239ba5bec3df5bf3fe
eyob6117 <eyobteklu911@gmail.com>
2021-06-28 05:03:59 -0700

    tasks on pointer

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,7 +10,7 @@ void rev_string(char *s)
 int len = strlen(s) - 1;
 int i;
 char tmp;
-for (i = 0; i < len / 2; i++)
+for (i = 0; i > len / 2; i--)
 tmp = s[i];
 s[i] = s[len - i];
 s[len - i] = tmp;
