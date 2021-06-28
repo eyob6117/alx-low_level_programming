5f1190f0990b80f3a4fad5578ca941c6cd28d4eb
eyob6117 <eyobteklu911@gmail.com>
2021-06-28 06:21:13 -0700

    tasks on pointer

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,22 +10,13 @@ void puts_half(char *str)
 {
 int len = strlen(str);
 int i;
-for (i = 0; i < len; i++)
+for (i = len / 2; i < len; i++)
 {
 if (i % 2 != 0)
 {
 len = (strlen(str) - 1) / 2;
-}
-else 
-
-if (i < len/2)
-{
-continue;
-}
-else
-{   
+}   
 _putchar(str[i]);
-}
 }
 _putchar('\n');
 }
