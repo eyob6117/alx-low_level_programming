0eee9faa90836805b8afd4f582f3df9110eb7ab4
eyob6117 <eyobteklu911@gmail.com>
2021-06-28 05:46:37 -0700

    tasks on pointer

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * puts_half - print half of a string,
+ * @str: pointer variable str
+ */
+
+void puts_half(char *str)
+{
+int len = strlen(str);
+int i;
+int n;
+for (i = 0; i < len; i++)
+{
+if (i % 2 != 0)
+{
+len = (strlen(str) - 1) / 2;
+}
+else 
+
+if (i < len/2)
+{
+continue;
+}
+else
+{   
+_putchar(str[i]);
+}
+}
+_putchar('\n');
+}
