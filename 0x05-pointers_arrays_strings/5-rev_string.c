67431289eea732145d0d81f0b049966dd5b09c81
eyob6117 <eyobteklu911@gmail.com>
2021-06-28 03:01:40 -0700

    tasks on pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * rev_string - function that reverse a string
+ * @s: variable pointer s
+ */
+
+void rev_string(char *s)
+{
+int len = strlen(s) - 1;
+int i;
+char tmp;
+for (i = 0; i > len/2; i--);
+{
+tmp = s[i];
+s[i] = s[len-i];
+s[len-i] = tmp;
+} 
