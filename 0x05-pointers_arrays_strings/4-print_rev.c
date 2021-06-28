6920386c95617a880bd7ea55da587e159bafe37e
eyob6117 <eyobteklu911@gmail.com>
2021-06-28 01:41:35 -0700

    tasks on pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_rev - a function that prints a string in reverse
+ * @s: pointer variable s
+ */
+void print_rev(char *s)
+{
+strrev(s);
+}
