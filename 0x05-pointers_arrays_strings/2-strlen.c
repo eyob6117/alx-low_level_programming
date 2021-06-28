5b0ee11d3b0891810643d868be5ad0d2258578d7
eyob6117 <eyobteklu911@gmail.com>
2021-06-28 00:45:01 -0700

    tasks on pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: character variable s
+ *
+ * Return: Always
+ */
+int _strlen(char *s)
+{
+int len = strlen(s);
+return (len);
+}
