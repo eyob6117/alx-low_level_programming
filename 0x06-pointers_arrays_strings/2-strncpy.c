38ddc75ae05d910c2744005a9baed3369f712f15
eyob6117 <eyobteklu911@gmail.com>
2021-06-30 02:49:20 -0700

    more tasks on pointer and arrays

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "hlberton.h"
+#include <stdio.h>
+/**
+ * *_strncpy - function that copies a string
+ * @dest: variable pointer dest
+ * @src: varable pointer src
+ * @n: integer variable n
+ * Retrun: to char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src, n);
+return (0);
+}
