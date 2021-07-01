7e828044a05f5006021bc2d914cd8f404e1b309b
eyob6117 <eyobteklu911@gmail.com>
2021-06-30 21:57:08 -0700

    tasks on pointer and arrays

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strcmp - function that compares two strings
+ * @s1: character pointer s1
+ * @s2: characteter pointer s2
+ * Return: to int
+ */
+int _strcmp(char *s1, char *s2)
+{
+strcmp(s1, s2);
+return (s1);
+}
