a68e30043279c8ae0ead36f289d8fc43d15d852b
eyob6117 <eyobteklu911@gmail.com>
2021-07-01 04:32:40 -0700

    tasks on pointer and array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,23 +1,18 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
- * reverse_array - function that reverses the content of an array integer
- * @a: variable pointed by integer a
- * @n: integer variable n
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: The array of integers to be reversed.
+ * @n: The number of elements in the array.
  */
 void reverse_array(int *a, int n)
 {
-int i;
-int tmp;
+int tmp, index;
 
-for (i = n - 1; i >= n/2; i--)
+for (index = n - 1; index >= n / 2; index--)
 {
-
-tmp = a[n - 1 - i];
-
-a[n - 1 - i] = a[i];
-
-a[i] = tmp;
-
+tmp = a[n - 1 - index];
+a[n - 1 - index] = a[index];
+a[index] = tmp;
 }
 }
