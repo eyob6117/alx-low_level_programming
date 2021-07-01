c03f454110ef4b85bb08cdff93e3e2fc7df6f8c5
eyob6117 <eyobteklu911@gmail.com>
2021-07-01 04:37:33 -0700

    tasks on pointer and array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,6 +4,7 @@
  * reverse_array - Reverses the content of an array of integers.
  * @a: The array of integers to be reversed.
  * @n: The number of elements in the array.
+ * Return: nothing
  */
 void reverse_array(int *a, int n)
 {
