80dc2cd83dc90dcb460e2f62a692cadf5cb89d83
eyob6117 <eyobteklu911@gmail.com>
2021-06-30 22:37:14 -0700

    tasks on pointer and arrays

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,13 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * _strcmp - function that compares two strings
- * @s1: character pointer s1
- * @s2: characteter pointer s2
- * Return: to int
+ * _strcmp - Compares pointers to two strings.
+ * @s1: A pointer to the first string to be compared.
+ * @s2: A pointer to the second string to be compared.
+ *
+ * Return: If str1 < str2, the negative difference of the first unmatched characters.
+ *         If str1 == str2, 0.
+ *         If str1 > str2, the positive difference of the first unmatched characters.
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -12,6 +15,6 @@ while (*s1 && *s2 && *s1 == *s2)
 {
 s1++;
 s2++;
-} 
+}
 return (*s1 - *s2);
-} 
+}
