863a46c495e5ca3ac78c905b586554e8c77e7feb
eyob6117 <eyobteklu911@gmail.com>
2021-06-30 21:50:43 -0700

    tasks on pointer and arrays

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,10 +5,10 @@
  * @dest: variable pointer dest
  * @src: varable pointer src
  * @n: integer variable n
- * Retrun: to char
+ * Return: to charcter dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 strncpy(dest, src, n);
-return (0);
+return (dest);
 }
