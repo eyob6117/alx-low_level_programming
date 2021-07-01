0bb0bb4df1eca0171fcb7d4ca9fb77fd17222cc6
eyob6117 <eyobteklu911@gmail.com>
2021-07-01 08:04:12 -0700

    tasks on pointer and arrays

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,11 +14,10 @@ while (str[indx1])
 for (indx2 = 0; indx2 <= 7; indx2++)
 {
 if (str[indx1] == leet[indx2] || str[indx1] - 32 == leet[indx2])
-{
+
 str[indx1] = indx2 + '0';
 }
 indx1++;
-}
 }
 return (str);
 }
