fdd9e06e4fb1a5062edda253bc5d319e56911dcf
eyob6117 <eyobteklu911@gmail.com>
2021-07-01 04:20:56 -0700

    tasks on pointer and array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,15 +5,19 @@
  * @a: variable pointed by integer a
  * @n: integer variable n
  */
-
 void reverse_array(int *a, int n)
 {
 int i;
 int tmp;
+
 for (i = n - 1; i >= n/2; i--)
 {
+
 tmp = a[n - 1 - i];
+
 a[n - 1 - i] = a[i];
+
 a[i] = tmp;
+
 }
 }
