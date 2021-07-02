31df2cca0c58e9d1f2a0669abff4cd1b68152507
eyob6117 <eyobteklu911@gmail.com>
2021-07-02 12:03:27 -0700

    tasks on pointer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,22 +1,34 @@
 #include "holberton.h"
-
+void print_unsigned_int(unsigned int n);
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * print_number - print number n with putchar
+ *
+ * @n: number to print
+ *
+ * Return: always void
  */
 void print_number(int n)
 {
-unsigned int num = n;
-
 if (n < 0)
 {
 _putchar('-');
-num = -num;
+print_unsigned_int(-(unsigned int)n);
 }
-
-if ((num / 10) > 0)
+else
+print_unsigned_int(n);
+}
+/**
+ * print_unsigned_int - prints an unsigned integer
+ *
+ * @n: an unsigned integer to print
+ *
+ * Return: always void
+ */
+void print_unsigned_int(unsigned int n)
 {
-print_number(num / 10);
+if (n / 10 != 0)
+{
+print_unsigned_int(n / 10);
 }
-_putchar((num % 10) + '0');
+_putchar((n % 10) + '0');
 }
