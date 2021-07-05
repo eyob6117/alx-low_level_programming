e95ef3de4fff9bb70f9e940423143289640cb01b
eyob6117 <eyobteklu911@gmail.com>
2021-07-05 03:20:09 -0700

    tasks on pointer arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,13 @@
  */
 char *_strchr(char *s, char c)
 {
-int i = 0;
-while (*(s + i))
+int index;
+
+for (index = 0; s[index] >= '\0'; index++)
 {
-if (*(s + i) == c)
-{
-return (s + i);
-i++:
+if (s[index] == c)
+return (s + index);
 }
-if (*(s + i) == c)
-return (s + i;
-}
-return (NULL):
+
+return ('\0');
 }
