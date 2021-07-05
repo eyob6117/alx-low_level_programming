9e61d1c4df04a1e932b21a138f6f542323e93770
eyob6117 <eyobteklu911@gmail.com>
2021-07-05 02:00:40 -0700

    tasks on arrays pointer and strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * *_memcpy - function that copies memory area
+ * @dest: variable pointer  dest
+ * @src: variable pointer src
+ * @n: integer variable n
+ *
+ * Retrun: to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+unsigned char *destination = dest;
+const  unsigned char *source = src;
+
+
+for (i = 0; i < n; i++)
+{
+destination[i] = source[i];
+}
+return (destination);
+}
