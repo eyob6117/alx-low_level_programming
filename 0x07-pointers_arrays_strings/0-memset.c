806551d8c1de3bf1c6f64b087e523f3f59102b7f
eyob6117 <eyobteklu911@gmail.com>
2021-07-05 03:01:39 -0700

    tasks on pointer arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 /**
  * *_memset - function that filss memory with a constant byte
- * 
+ *
  * @s: character variable pointer
  * @b: character variable b
  * @n: unsigned int n
- * 
+ *
  * Return: to character s
  */
 char *_memset(char *s, char b, unsigned int n)
