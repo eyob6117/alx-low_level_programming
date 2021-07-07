3e9a816221bb3e218dd627ea66d5d4c0eefe8d97
eyob6117 <eyobteklu911@gmail.com>
2021-07-07 14:05:41 -0700

    tasks on recursive

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,29 +1,33 @@
 #include "holberton.h"
-int squareroot(int n, int i);
+
 /**
- * _sqrt_recursion - no loop, 1. 2nd function - checks for perfect square
- * @n: input
- * Return: Always 0 (Success)
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: int number
+ * Return: If no natural square root, return -1. Else return natural
+ * square root
  */
+
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-return (-1);
-else
-return (squareroot(n, (n + 1) / 2));
+return (halp(n, 1));
 }
+
 /**
- * squareroot - checks if perfect square
- * @n: input
- * @i: counter
- * Return: if square root
+ * halp - helper function to solve _sqrt_recursion
+ * @c: number to determine if square root
+ * @i: incrementer to compare against `c`
+ * Return: square root if natural square root, or -1 if none found
  */
-int squareroot(int n, int i)
+
+int halp(int c, int i)
 {
-if (i < 1)
-return (-1);
-else if (i * i == n)
+int square;
+
+square = i * i;
+if (square == c)
 return (i);
+else if (square < c)
+return (halp(c, i + 1));
 else
-return (squareroot(n, i - 1));
+return (-1);
 }
