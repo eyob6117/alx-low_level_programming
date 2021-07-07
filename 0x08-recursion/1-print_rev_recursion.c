45e6891565a5d733bc81d73f8a16af0e466ef68b
eyob6117 <eyobteklu911@gmail.com>
2021-07-07 11:39:18 -0700

    tasks on recursive

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,11 +6,13 @@
 
 void _print_rev_recursion(char *s)
 {
-int i = 0;
-if (s[i] == 0)
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1));
+}
+else
 {
 return;
 }
 _putchar(*s);
-_print_rev_recursion(s - 1);
 }
