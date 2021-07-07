a9ec34073e997a5cc26b29a716c03fbca5a2e37b
eyob6117 <eyobteklu911@gmail.com>
2021-07-07 12:03:59 -0700

    tasks on recursive

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,15 +7,12 @@
  */
 int _strlen_recursion(char *s)
 {
-int n = 0;
-if (*s == '\n')
+if (*s == '\0')
 {
-break;
+return (0);
 }
 else
 {
-n = n + _strlen_recursion(s + 1);
-
+return (1 + _strlen_recursion(s + 1));
 }
-return (n);
 }
