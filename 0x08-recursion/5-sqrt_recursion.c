0f5ecdddb68611871c68a9763a2ead2b31f53a85
eyob6117 <eyobteklu911@gmail.com>
2021-07-07 13:47:43 -0700

    tasks on recursive

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,13 +8,9 @@ int squareroot(int n, int i);
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-{
 return (-1);
-}
 else
-{
 return (squareroot(n, (n + 1) / 2));
-}
 }
 /**
  * squareroot - checks if perfect square
@@ -25,15 +21,9 @@ return (squareroot(n, (n + 1) / 2));
 int squareroot(int n, int i)
 {
 if (i < 1)
-{
 return (-1);
-}
 else if (i * i == n)
-{
 return (i);
-}
 else
-{
 return (squareroot(n, i - 1));
-}
 }
