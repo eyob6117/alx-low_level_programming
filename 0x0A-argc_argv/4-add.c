7a089b7feda444bab13f7448447ac2abdea618dc
eyob6117 <eyobteklu911@gmail.com>
2021-07-12 11:24:43 -0700

    tasks on argc and argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-#include "holberton.h"
 #include <stdlib.h>
+#include "holberton.h"
 /**
- * main - program that adds positive numbers
- * @argc: integer variable
- * @argv: variaple pointer
- * Return: Always(0)
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-int i;
-int sum = 0;
+int i, sum = 0;
+
 if (argc < 1)
-{
 return (0);
-}
+
 for (i = 1; i < argc; i++)
 {
 if (!atoi(argv[i]))
@@ -26,5 +24,6 @@ return (1);
 sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
+
 return (0);
 }
