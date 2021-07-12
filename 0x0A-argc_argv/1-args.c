e897da630c4baf2e791b621da18b95d7831ee221
eyob6117 <eyobteklu911@gmail.com>
2021-07-12 06:19:52 -0700

    tasks on argc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program that prints the number of argument
+ * @argc: integer variable
+ * @argv: pointer variable with array
+ * Return: Always(0) sucess
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d", argc);
+
+return (0);
+}
