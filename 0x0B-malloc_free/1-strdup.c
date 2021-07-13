cbeca7a77c690f97c361e0e90113aae51147d798
eyob6117 <eyobteklu911@gmail.com>
2021-07-13 05:11:17 -0700

    tasks on malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,11 +7,24 @@
  */
 char *_strdup(char *str)
 {
-char *array;
-unsigned int i;
+char *duplicate;
+int index, len = 0;
+
 if (str == NULL)
-return(NULL);   
-for (i = 0; str[i] < '\0'; i++)
-array[i] = str[i];
-return (array);
+return (NULL);
+
+for (index = 0; str[index]; index++)
+len++;
+
+duplicate = malloc(sizeof(char) * (len + 1));
+
+if (duplicate == NULL)
+return (NULL);
+
+for (index = 0; str[index]; index++)
+duplicate[index] = str[index];
+
+duplicate[len] = '\0';
+
+return (duplicate);
 }
