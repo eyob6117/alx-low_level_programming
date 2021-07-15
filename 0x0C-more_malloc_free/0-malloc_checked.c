949a383abd0bee2e01ffd7262f439c4e6f87f54f
eyob6117 <eyobteklu911@gmail.com>
2021-07-15 11:39:33 -0700

    tasks on more malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,7 +4,7 @@
 /**
  * *malloc_checked - function that allocates memory using malloc
  * @b: integer variable
- * 
+ *
  * Return: A pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
