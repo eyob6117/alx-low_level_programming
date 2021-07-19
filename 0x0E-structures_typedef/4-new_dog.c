ecd75b48e7ebe82e83d10bb71a439e7f379c76f5
eyob6117 <eyobteklu911@gmail.com>
2021-07-19 14:14:59 -0700

    tasks on struct and typdef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * *new_dog - function that create a new dog
+ * @name: character pointer variable
+ * @age: float variable
+ * @owner: charcter pointer variable
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+if (new_dog == NULL)
+return (NULL);
