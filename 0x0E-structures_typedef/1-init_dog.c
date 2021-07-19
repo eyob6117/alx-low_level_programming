60fa6a8143c0e6db97715f3e848f614faddaab41
eyob6117 <eyobteklu911@gmail.com>
2021-07-19 13:28:57 -0700

    tasks on struct and typdef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,19 +1,24 @@
-#include "dog.h"
 #include <stdlib.h>
-
+#include <stdio.h>
+#include "dog.h"
 /**
- * init_dog - Initializes a variable of type struct dog.
- * @d: The dog to be initialized.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * init_dog - a function that initialize a variable of type struct dog
+ * @d: struct name
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: 0
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d != NULL)
+if (d == NULL)
 {
+d = malloc(sizeof(struct dog));
+if (d == NULL)
+return;
+}
+
 d->name = name;
 d->age = age;
 d->owner = owner;
-}
 }
