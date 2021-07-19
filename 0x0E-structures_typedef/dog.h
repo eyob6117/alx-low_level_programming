bd9b824bd76462fd3ea4d21bbc34fcf80af77a48
eyob6117 <eyobteklu911@gmail.com>
2021-07-19 12:45:35 -0700

    tasks on struct and typdef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,11 @@
+/**
+ * struct dog - declaration of struct
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: declaring struct and intialize with inside scope
+ */
 struct dog
 {
 char *name;
