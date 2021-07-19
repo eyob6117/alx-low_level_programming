9e3365d208ec97434381851f55dab25aa294fd50
eyob6117 <eyobteklu911@gmail.com>
2021-07-19 13:18:04 -0700

    tasks on struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,15 +7,14 @@
  * @name: character pointer name
  * @age: float variable
  * @owner: character pointer variable
- * Return: d
+ * Return: 0
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d == NULL)
-{
 d = malloc(sizeof(struct dog));
+if (d == NULL)
 return;
-}
 d->name = name;
 d->age = age;
 d->owner = owner;
