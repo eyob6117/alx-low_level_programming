657adc53cefc5ad5716699dec01001e9835f4246
eyob6117 <eyobteklu911@gmail.com>
2021-07-20 11:44:13 -0700

    tasks on pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * print_name - function that prints a name
  * @name: pointer varible to char
@@ -6,8 +8,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-f = &print_name_as_is;
-f = &print_name_uppercase;
-name = f;
-printf("%s", name);
+if (name == NULL || f == NULL)
+return;
+f(name);
 }
