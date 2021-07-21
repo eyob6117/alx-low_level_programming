a3bfa2975b130fc5f88b8524c3ecf238675c3548
eyob6117 <eyobteklu911@gmail.com>
2021-07-21 08:05:50 -0700

    tasks on function pointer

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -30,8 +30,7 @@ printf("Error\n");
 exit(99);
 }
 
-if ((*op == '/' && num2 == 0) ||
-(*op == '%' && num2 == 0))
+if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 {
 printf("Error\n");
 exit(100);
