87be3587460fc9d009ee8b93ddb59c592b9fe750
eyob6117 <eyobteklu911@gmail.com>
2021-07-21 03:24:38 -0700

    tasks on pointer funciton

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,15 +11,15 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 
-unsigned int i;
-int temp;
+int i;
 if (array == NULL || cmp == NULL)
-return;
+return (-1);
 if (size <= 0)
 return (-1);
 for (i = 0; i < size; i++)
+{
 if (cmp(array[i] != 0)
 return (i);
-else
+}
 return (-1);
 }
