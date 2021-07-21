27767bffdb0b4d2ac0e598e79f344341a78d2c50
eyob6117 <eyobteklu911@gmail.com>
2021-07-21 02:02:11 -0700

    tasks on function pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointer.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * array_iterator - executes function a given parmaeter
+ * @array: argument 1
+ * @size: integer variable
+ * @action: pointer variable
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+if (array == NULL || action == NULL)
+return;
+int i = 0;
+while (i < size)
+{
+action(array[i])
+i++;
+}
