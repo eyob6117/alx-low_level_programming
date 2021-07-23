b9e20a4a35ad820df25572ee6a1f0c4cc0091ae2
eyob6117 <eyobteklu911@gmail.com>
2021-07-23 13:43:37 -0700

    tasks on function pointer

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,7 +8,13 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-op_t op_s[] = {{"+", op_add}, {"-", op_sub}, {"*", op_mul}, {"/", op_div}, {"%", op_mod}, {NULL, NULL}};
+op_t op_s[] = {
+	       {"+", op_add},
+	       {"-", op_sub},
+	       {"*", op_mul},
+	       {"/", op_div},
+	       {"%", op_mod},
+	       {NULL, NULL}};
 
 int i = 0;
 
